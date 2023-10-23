85a702405fba867c6d7d6eaf7ec5f489897c1bca
Petar Georgiev <petiyo07@abv.bg>
2023-10-24 01:41:50 +0300

    Added the store

---
+++ src/store/index-store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import toggleSlice from "./toggle-slice";
+const store = configureStore({
+  reducer: { toggle: toggleSlice.reducer },
+});
+
+export default store;
