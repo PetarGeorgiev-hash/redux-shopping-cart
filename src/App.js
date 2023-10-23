d800b392c7c5cfc55297123a67c5ad1af03e1954
Petar Georgiev <petiyo07@abv.bg>
2023-10-24 00:58:03 +0300

    Added the header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./Header/Header";
 import Items from "./items-component/Items";
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Items></Items>
     </div>
   );
